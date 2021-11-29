d6f5884742455bef4679f4c655133632c4b6d188
INDER-S <indersonuzomato@gmail.com>
2021-11-30 00:48:36 +0530

    Added basic functions

---
+++ MediaPlayer/src/main/java/com/example/mediaplayer/Backward.java
@@ -0,0 +1,2 @@
+package com.example.mediaplayer;public class Backward {
+}
--- MediaPlayer/src/main/java/com/example/mediaplayer/Main.java
+++ MediaPlayer/src/main/java/com/example/mediaplayer/Main.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Player.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Hello!");
+        stage.setTitle("Group19_Mediaplayer");
         stage.setScene(scene);
         stage.show();
     }
